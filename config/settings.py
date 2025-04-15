a1bac5d878f68622dc2db0ed47e91a2f11528b53
Twiiiz <tymur.mustaev@gmail.com>
2025-04-15 13:04:14 +0300

    Created new app: "detection"

--- config/settings.py
+++ config/settings.py
@@ -32,12 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #Djano Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Project Apps
+    'apps.detection'
 ]
 
 MIDDLEWARE = [
