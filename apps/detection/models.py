469e632541b793cbd59a24624ca1bda12c18a55f
Twiiiz <tymur.mustaev@gmail.com>
2025-05-07 13:30:48 +0300

    create Attempt model in detection

--- apps/detection/models.py
+++ apps/detection/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Attempt(models.Model):
+    attempt_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE, blank=True, null=True)
+    news_text = models.TextField()
+    model = models.ForeignKey(to="admin_control.Model", on_delete=models.CASCADE)
+    result = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- apps/detection/views.py
+++ apps/detection/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .forms import NewsDetectionForm
 from ..admin_control.classificator import ModelManager
+from .models import Attempt
 
 class DetectionView(View):
     def get(self, request):
@@ -15,6 +16,13 @@ class DetectionView(View):
             model = form.cleaned_data.get("model")
             model_manager = ModelManager()
             result = model_manager.detect_fake(text, model)
+            attempt = Attempt()
+            if request.user.is_authenticated:
+                attempt.user = request.user
+            attempt.news_text = text
+            attempt.model = model
+            attempt.result = result
+            attempt.save()
             return render(request, "index.html", {"result": result[0]})
 
         return redirect("index")
